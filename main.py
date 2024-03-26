634cf3c5916bcd3c64e6c6d611820d98966c6967
mansurbek046 <mansurbek046@gmail.com>
2024-03-26 16:56:38 +0500

    update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from credentials import api_hash, api_id, bot_token
 import logging
 from pyrogram import Client, filters
 
-app=Client(api_hash=api_hash, api_id=api_id, bot_token=bot_token)
+app=Client('mussaid_bot', api_hash=api_hash, api_id=api_id, bot_token=bot_token)
 
 @app.on_message(filters.command('start'))
 async def welcome(client, message):
