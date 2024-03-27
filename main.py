b8e67d5b4b2ece4c57a75edd4f850ff5b29fc06d
mansurbek046 <mansurbek046@gmail.com>
2024-03-27 17:05:57 +0500

    update main.py

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 from credentials import api_hash, api_id, bot_token
 import logging
 from pyrogram import Client, filters
+from urllib.parse import urlparse, parse_qs
 
 app=Client('mussaid_bot', api_hash=api_hash, api_id=api_id, bot_token=bot_token)
 
 @app.on_message(filters.command('start'))
 async def welcome(client, message):
   chat_id=message.chat.id
-  await client.send_message(chat_id=chat_id, text="hi")
+  await client.send_message(chat_id=chat_id, text="Welcome! Send me media link of Instagram, X and Tiktok or Telegram Story to download...")
 
+@app.on_message()
+async def handler(client, message):
+  chat_id=message.chat.id
+  networks=[
+    "www.instagram.com",
+    "instagram.com",
+    "x.com",
+    "www.x.com",
+    "mobile.twitter.com",
+    "tiktok.com",
+    "www.tiktok.com",
+    "t.me",
+    "telegram.me"
+  ]
+  if message.text:
+    urls=message.text.split()
+    # await message.delete()
+    count=len(urls)
+    for url in urls:
+      parsed_url=urlparse(url)
+      
+      if parsed_url.netloc in networks:
+        count=-1
+        if "instagram" in str(parsed_url.netloc):
+          client.send_message(chat_id=chat_id, text="Instagram")
+  
+    if count=len(urls):
+      await client.send_message(chat_id=chat_id, text="Send me link from Instagram, X, TikTok or Telegram Story...")
+  
+  await client.send_message(chat_id=chat_id, "message")
 
 if __name__=="__main__":
   app.run()
